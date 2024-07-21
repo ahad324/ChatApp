9592661f3e18a5f178e799f3add2c285732dd09e
ahad <ahadg446@gmail.com>
2024-07-21 10:36:47 +0500

    Added _redirects

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import path from 'path';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
